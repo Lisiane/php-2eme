4a29b307acf06c0441fdf0186ef88c15e476f49b
Lisiane <lilirapin@gmail.com>
2015-04-21 21:11:44 +0200

    Exercice 5
    J'ai pas tout réussi monsieur Lambercy... désolée

---
+++ exercice5/devine.php
@@ -0,0 +1,35 @@
+<?php
+/* ---------------------------------------------------------------------
+ * Exercice 5 chapitre 2                            
+ * Nom du fichier: devine.php
+ * Auteur: LR 
+ * Date: 21 avril 2015
+ * Description: c'est une devinette !                            
+ * ---------------------------------------------------------------------
+ */
+// Variables
+	$nombre = 0; 
+	CONST = $nbr_max; 
+	CONST = $nbr_min; 
+//  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Exercice 5</title>
+  </head>
+  <body>
+
+  <h2> Jeu de devinette </h2>
+  <form name="form1" method="post" action="titre">
+    <p>Deviner le nombre entre 0 et 5<br>
+      <input type="text" name="nombre" />
+    </p>
+    <p>
+      <input type="submit" name="Submit" value="S'inscrire">
+      <br>
+    </p>
+  </form>
+</body>
+</html>
